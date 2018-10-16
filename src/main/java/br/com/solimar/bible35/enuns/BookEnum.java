c35f8d1dc9289e8365ca16b7c8cf293feac0e790
Solimar Silva <solimar.silva@trt8.jus.br>
2018-10-16 14:00:13 -0300

    ...

--- src/main/java/br/com/solimar/bible35/enuns/BookEnum.java
+++ src/main/java/br/com/solimar/bible35/enuns/BookEnum.java
@@ -2,6 +2,32 @@ package br.com.solimar.bible35.enuns;
 
 public enum BookEnum {
 
-	GN, EX
+	GN, 
+	EX,
+	LV,
+	NM,
+	DT,
+	JS,
+	JZ,
+	RT,
+	ISM,
+	IISM,
+	IRS,
+	IIRS,
+	ICR,
+	IICR,
+	ED,
+	NE,
+	ET,
+	JOH,
+	SL,
+	PV,
+	EC,
+	CT,
+	IS,
+	JR
+	
+	
+	
 
 }
